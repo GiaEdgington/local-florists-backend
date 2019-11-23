c9a44f245d79a05ec6d0ab25dfc04f78afe6d23d
Gia Edgington <gia.edgington@gmail.com>
2019-11-23 15:02:37 -0500

    Add files via upload

--- florists.js
+++ florists.js
@@ -2,20 +2,34 @@
 
 const express = require('express');
 const router = express.Router();
-const yelp = require('yelp-fusion');
+const yelp = require('yelp-fusion'); //Yelp Fusion API reference
+
 require('dotenv/config');
 
 const apiKey= process.env.API_KEY;
-
 const client = yelp.client(apiKey);
 
 router.get('/', (req, res) => {
 
-    //SUBMIT SEARCH REQUEST
-    const searchRequest =  {
-        term: 'Florists',
-        location: req.query.location,
-    };
+    let searchRequest = {}
+
+    //Determine if request is location or coordinate based search and set parameters.
+
+    if (req.query.location) {
+        searchRequest = {
+            term: 'Florists',
+            location: req.query.location,
+        };
+    }
+    else if (req.query.latitude && req.query.longitude) {
+        searchRequest = {
+            term: 'Florists',
+            latitude: req.query.latitude,
+            longitude: req.query.longitude
+        };
+    }
+
+    //Submit Yelp API search with request.
 
     client.search(searchRequest).then(response => {
         const responseJson = JSON.stringify(response.jsonBody.businesses, null, 4);
@@ -25,6 +39,5 @@ router.get('/', (req, res) => {
     });
 
 });
-
 
 module.exports = router;
