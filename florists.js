d705e5ddae8d9cc67bb1bc238c8710ce55391f26
Gia Edgington <gia.edgington@gmail.com>
2019-11-23 13:19:45 -0500

    get api request

--- florists.js
+++ florists.js
@@ -2,6 +2,29 @@
 
 const express = require('express');
 const router = express.Router();
-const yelp = require('yelp fusion');
+const yelp = require('yelp-fusion');
 require('dotenv/config');
 
+const apiKey= process.env.API_KEY;
+
+const client = yelp.client(apiKey);
+
+router.get('/', (req, res) => {
+
+    //SUBMIT SEARCH REQUEST
+    const searchRequest =  {
+        term: 'Florists',
+        location: req.query.location,
+    };
+
+    client.search(searchRequest).then(response => {
+        const responseJson = JSON.stringify(response.jsonBody.businesses, null, 4);
+        res.send(responseJson);
+    }).catch(e => {
+        res.send(e);
+    });
+
+});
+
+
+module.exports = router;
