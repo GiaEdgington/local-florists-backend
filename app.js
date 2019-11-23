a70621469485d23db161e45ddcb462891da7e4d4
Gia Edgington <gia.edgington@gmail.com>
2019-11-23 13:49:59 -0500

    cors required

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
+
+//Middleware
+app.use(cors());
+app.use('/florists', floristsRoute);
 
 //ROUTE
 const floristsRoute = require('./florists');
-
-app.use('/florists', floristsRoute);
-
-app.get('/', (req, res) => {
-    res.send('We are on home');
-});
 
 
 app.listen(3000);
